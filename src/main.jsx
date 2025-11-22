50e958727a38bc803ec8b38a593bfed8b84e15f6
Nopy <nopy837@gmail.com>
2025-11-22 15:56:02 +0100

    Fix import path for SurveyForm component

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import SurveyForm from '../app.jsx'
+import SurveyForm from './app.jsx'  // ← CORRIGÉ
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
