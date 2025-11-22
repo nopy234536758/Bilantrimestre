21431adc371d6ea4f3fe75fce623c713c111850a
Nopy <nopy837@gmail.com>
2025-11-22 15:57:03 +0100

    Change base path in Vite config to './'

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Changez ceci si votre repo a un nom spécifique
+  base: './',  // ← Changez pour './'
   build: {
     outDir: 'dist',
     sourcemap: true
